63f8b51ad2a9cc56c59a5b449bb652d4ae229004
Mridul Agarwal <mridul310703@gmail.com>
2025-02-06 17:14:41 +0530

    .

--- blockchain_lib.py
+++ blockchain_lib.py
@@ -1,19 +1,26 @@
 import heapq
 from enum import Enum, auto
+from typing import Union, Dict
 
 class EventType(Enum):
     GENERATE_TRANSACTION = auto()
     RECEIVE_TRANSACTION = auto()
+    END_MINING = auto()
+    RECEIVE_BLOCK = auto()
     
-    
+COINBASE_REWARD = 50  # 50 coins
+
 class Transaction:
-    def __init__(self, tx_id, sender: int, receiver: int, amount: int):
+    def __init__(self, tx_id, sender: int, receiver: int, amount: int, timestamp):
         self.tx_id = tx_id
         self.sender = sender
         self.receiver = receiver
         self.amount = amount
+        self.timestamp = timestamp
     
     def __str__(self):
+        if self.sender is None:
+            return f"{self.tx_id}: {self.receiver} mines {self.amount} coins"
         return f"{self.tx_id}: {self.sender} pays {self.receiver} {self.amount} coins"
     
     def __eq__(self, other: "Transaction"):
@@ -24,9 +31,12 @@ class Transaction:
     def __hash__(self):
         return hash(self.tx_id)
     
+    def __lt__(self, other: "Transaction"):
+        return self.timestamp < other.timestamp
+    
 
 class Event:
-    def __init__(self, timestamp, event_type: EventType, data: Transaction, sender: int, receiver: int):
+    def __init__(self, timestamp, event_type: EventType, data: Union[Transaction, 'Block'], sender: int, receiver: int):
         self.timestamp = timestamp
         self.event_type = event_type
         self.data = data
@@ -48,4 +58,72 @@ class EventQueue:
         return heapq.heappop(self.queue)
     
 
+class Block:
+    def __init__(self, block_id, transactions: list[Transaction], parent_block_id, timestamp):
+        self.block_id = block_id
+        self.transactions = transactions
+        self.create_timestamp = timestamp
+        self.parent_block_id = parent_block_id
+
+class BlockChainNode:
+    def __init__(self, block: Block, parent: "BlockChainNode"):
+        self.block = block
+        self.children: list[BlockChainNode] = []
+        self.parent = parent
+        self.height = self.parent.height + 1 if self.parent else 0
+        self.balance_map: Dict[int, int] = {}
+        ## update balance map
+        if self.parent:
+            self.balance_map = self.parent.balance_map.copy()
+            for tx in block.transactions:
+                if tx.sender is not None:
+                    self.balance_map[tx.sender] -= tx.amount
+                self.balance_map[tx.receiver] += tx.amount
+        else:
+            self.balance_map = {tx.sender: 0 for tx in block.transactions} ## SW
+
+class BlockchainTree:
+    def __init__(self):
+        self.genesis_block = Block(0, [], -1)
+        self.root = BlockChainNode(self.genesis_block, None)
+        self.buffer: list[Block] = [] # Buffer for blocks that are not yet part of the blockchain
+        self.nodes: Dict[int, BlockChainNode] = {0: self.root}
+        self.longest_chain_leaf = self.root
+        self.height = 0
+
+    def add(self, block: Block) -> Block:
+        self.buffer.append(block)
+        prev_buffer_len = -1
+        while len(self.buffer) != prev_buffer_len:
+            prev_buffer_len = len(self.buffer)
+            for block in self.buffer:
+                if block.parent_block_id not in self.nodes:
+                    continue
+                ## validate block, if failed, remove from buffer, continue, else add to blockchain
+                ## check if coinbase transaction is valid
+                if block.transactions[0].amount != COINBASE_REWARD  # 50 coins
+                    remove_block_and_its_children(block)
+                    continue
+                ## check if tranactions in this block are not there in the chain in which it is being added
+                for tx in block.transactions:
+
+                    ## check if final balances dont go negative for any peer
+                    ## check if block timestamp is greater than parent block timestamp
+                
+
+                ## add tx_ids of block in own seen_txs if added on longest chain and remove those from our mempool
+
+
+                parent_node = self.nodes[block.parent_block_id]
+                new_node = BlockChainNode(block, parent_node)
+                self.nodes[block.block_id] = new_node
+                parent_node.children.append(new_node)
+                if new_node.height > self.height:
+                    self.height = new_node.height
+                    self.longest_chain_leaf = new_node
+            self.buffer = [block for block in self.buffer if block.block_id not in self.nodes]
+
+        
+            
     
+
--- simulator.py
+++ simulator.py
@@ -2,9 +2,11 @@ import random
 import argparse
 import uuid
 from blockchain_lib import *
+from typing import Set, Union, Dict
 
 ## global constants
 TX_SIZE = 1/1024  # 1 KB (in MB)
+COINBASE_REWARD = 50  # 50 coins
 
 random.seed(42)
 
@@ -15,36 +17,82 @@ class Peer:
         self.balance = balance
         self.is_slow = is_slow
         self.is_low_cpu = is_low_cpu
-        self.blockchain = None
-        self.mem_pool: set[Transaction] = set()
+        self.mem_pool: list[Transaction] = []
         self.interarrival_time = interarrival_time
         self.neighbours: list[Peer] = []
+        self.transactions_seen: Set[uuid.UUID] = set() ## add tx when generated, received by peer, or added to blockchain
+        self.blocks_seen: Set[uuid.UUID] = set() ## add block when received by peer or added to blockchain
+        self.blockchain_tree = BlockchainTree()
+        self.block_being_mined = None
 
-    def generate_transaction(self, network: "P2PNetwork", timestamp):
-
+    def generate_transaction(self, timestamp, peers: list["Peer"]) -> Event:
         delay = random.expovariate(1.0 / self.interarrival_time)
-        receiver = random.choice([peer for peer in network.peers if peer.peer_id != self.peer_id])
+        receiver = random.choice([peer for peer in peers if peer.peer_id != self.peer_id])
         amount = random.randint(1, self.balance) # Update where? global amount map SW
         tx_id = uuid.uuid4()
-        transaction = Transaction(tx_id, self.peer_id, receiver.peer_id, amount)
+        transaction = Transaction(tx_id, self.peer_id, receiver.peer_id, amount, timestamp + delay)
         event = Event(timestamp + delay, EventType.GENERATE_TRANSACTION, transaction, self.peer_id, None)
-        network.event_queue.add_event(event)
+        self.transactions_seen.add(tx_id)
+        return event
 
-        for neighbour in self.neighbours:
-            latencies = network.latencies[(self.peer_id, neighbour.peer_id)]
-            rho = latencies["rho"]
-            c = latencies["c"]
-            d = random.expovariate((12/1024) / c)
-            receiver_delay = rho + TX_SIZE / c + d + delay
-            event = Event(timestamp + receiver_delay, EventType.RECEIVE_TRANSACTION, transaction, self.peer_id, neighbour.peer_id)
-            network.event_queue.add_event(event)
+    def start_mining(self, timestamp, hashing_power: float, interarrival_time: int) -> Event:
+        parent_block_id = self.blockchain_tree.longest_chain_leaf.block.block_id
+        coinbase_transaction = Transaction(uuid.uuid4(), None, self.peer_id, COINBASE_REWARD, timestamp)
+        block_transactions = [coinbase_transaction] + self.choose_transactions()
+        block_id = uuid.uuid4()
+        mining_time = random.expovariate(hashing_power / interarrival_time)
+        block = Block(block_id, block_transactions, parent_block_id, timestamp + mining_time)
+        self.block_being_mined = block
+        event = Event(timestamp + mining_time, EventType.END_MINING, block, self.peer_id, None)
+        return event
+    
+    def mine(self, block: Block):
+        self.blockchain_tree.add(block) ## balance map updated
+        self.block_being_mined = None
+        self.blocks_seen.add(block.block_id)
+        ## remove txs from mempool
+        self.mem_pool = [tx for tx in self.mem_pool if tx.tx_id not in block.transactions]
+
+
+    def choose_transactions(self) -> list[Transaction]:
+        ## also updates balances of all peers
+        transactions: list[Transaction] = []
+        balance_map = self.blockchain_tree.longest_chain_leaf.balance_map.copy()
+        for tx in sorted(self.mem_pool):
+            if len(transactions) == 999:
+                break
+            if balance_map[tx.sender] >= tx.amount:
+                transactions.append(tx)
+                balance_map[tx.sender] -= tx.amount
+                balance_map[tx.receiver] += tx.amount  
+        return transactions
+
+    def receive_transaction(self, transaction: Transaction) -> bool:
+        if transaction.tx_id in self.transactions_seen:
+            return False
+        self.mem_pool.append(transaction)
+        self.transactions_seen.add(transaction.tx_id)
+        return True
+    
+    def receive_block(self, block: Block) -> bool:
+        ## validate transactions
+        ## append in own tree
+        ## check if already mining and if need to change the block being mined on
+
+        if block.block_id in self.blocks_seen:
+            return False
+        self.blockchain_tree.add(block)
+        return True
 
 class P2PNetwork:
-    def __init__(self, num_peers: int, frac_slow: bool, frac_low_cpu: bool, interarrival_time: float): ## z0 is frac_slow, z1 is frac_low_cpu
+    def __init__(self, num_peers: int, frac_slow: bool, frac_low_cpu: bool, interarrival_time: float, I: float): ## z0 is frac_slow, z1 is frac_low_cpu
         self.peers: list[Peer] = []
         self.num_peers = num_peers
         self.frac_slow = frac_slow
         self.frac_low_cpu = frac_low_cpu
+        self.I = I
+        self.low_hashing_power = 1 / (num_peers * (10 - 9 * frac_low_cpu))
+        self.high_hashing_power = 10 / (num_peers * (10 - 9 * frac_low_cpu))
         self.interarrival_time = interarrival_time
         self.initialize_event_queue()
         self.create_peers()
@@ -57,23 +105,12 @@ class P2PNetwork:
         self.event_queue = EventQueue()
         for i in range(self.num_peers):
             sender = self.peers[i]
-            timestamp = random.expovariate(1.0 / self.interarrival_time)
-            
-            receiver = random.choice([peer for peer in self.peers if peer.peer_id != sender.peer_id])
-            amount = random.randint(1, sender.balance) # Update where? global amount map SW
-            tx_id = uuid.uuid4()
-            transaction = Transaction(tx_id, sender.peer_id, receiver.peer_id, amount)
-            event = Event(timestamp, EventType.GENERATE_TRANSACTION, transaction, sender.peer_id, None)
-            self.event_queue.add_event(event)
-
-            for neighbour in self.peers[i].neighbours:
-                latencies = network.latencies[(sender.peer_id, neighbour.peer_id)]
-                rho = latencies["rho"]
-                c = latencies["c"]
-                d = random.expovariate((12/1024) / c)
-                delay = rho + TX_SIZE / c + d
-                event = Event(timestamp + delay, EventType.RECEIVE_TRANSACTION, transaction, sender.peer_id, neighbour.peer_id)
-                network.event_queue.add_event(event)
+            gen_tx_event = sender.generate_transaction(0, self.peers)
+            self.event_queue.add_event(gen_tx_event)
+            self.forward_packet(sender, gen_tx_event.data, gen_tx_event.timestamp, i)
+            hashing_power = self.low_hashing_power if sender.is_low_cpu else self.high_hashing_power
+            end_mining_event = sender.start_mining(0, hashing_power, self.I)
+            self.event_queue.add_event(end_mining_event)
 
     def create_peers(self):
         for i in range(self.num_peers):
@@ -124,6 +161,20 @@ class P2PNetwork:
                         "c": link_speed,
                     }
                     self.latencies[(neighbor.peer_id, peer.peer_id)] = self.latencies[(peer.peer_id, neighbor.peer_id)]
+    
+    def forward_packet(self, peer: Peer, data: Union[Transaction, Block], timestamp, sender_id: int):
+        for neighbour in peer.neighbours:
+            if neighbour.peer_id == sender_id:
+                continue
+            latencies = self.latencies[(peer.peer_id, neighbour.peer_id)]
+            rho = latencies["rho"]
+            c = latencies["c"]
+            d = random.expovariate((12/1024) / c)
+            message_size = 1 if isinstance(data, Transaction) else len(data.transactions) ## number of KBs
+            receiver_delay = rho + message_size * TX_SIZE / c + d
+            event_type = EventType.RECEIVE_TRANSACTION if isinstance(data, Transaction) else EventType.RECEIVE_BLOCK
+            event = Event(timestamp + receiver_delay, event_type, data, peer.peer_id, neighbour.peer_id)
+            self.event_queue.add_event(event)
 
     def process_events(self):
         while self.continue_simulation:
@@ -133,45 +184,67 @@ class P2PNetwork:
                     self.process_generate_transaction(event)
                 elif event.event_type == EventType.RECEIVE_TRANSACTION:
                     self.process_receive_transaction(event)
+                elif event.event_type == EventType.END_MINING:
+                    self.process_end_mining(event)
+                elif event.event_type == EventType.RECEIVE_BLOCK:
+                    self.process_receive_block(event)
     
     def process_generate_transaction(self, event: Event):
         id = event.sender
         timestamp = event.timestamp
         peer = self.peers[id]
-        peer.generate_transaction(self, timestamp)
+        gen_tx_event = peer.generate_transaction(self, timestamp, self.peers)
+        self.event_queue.add_event(gen_tx_event) ## add to event queue to generate next transaction
+        self.forward_packet(peer, gen_tx_event.data, gen_tx_event.timestamp, id)
 
     def process_receive_transaction(self, event: Event):
-        transaction = event.data
         receiver = self.peers[event.receiver]
-        if transaction in receiver.mem_pool: # or in blockchain SW
+        transaction = event.data
+        unseen_tx = receiver.receive_transaction(transaction)
+        if unseen_tx:
+            self.forward_packet(receiver, transaction, event.timestamp, event.sender)
+
+    def process_end_mining(self, event: Event):
+        id = event.sender
+        peer = self.peers[id]
+        block = event.data
+        hashing_power = self.low_hashing_power if peer.is_low_cpu else self.high_hashing_power
+        if block.id != peer.block_being_mined.id:
+            end_mining_event = peer.start_mining(event.timestamp, hashing_power, self.I)
+            self.event_queue.add_event(end_mining_event)
             return
-        receiver.mem_pool.append(transaction)
-        timestamp = event.timestamp
-        for neighbour in receiver.neighbours:
-            latencies = self.latencies[(receiver.peer_id, neighbour.peer_id)]
-            rho = latencies["rho"]
-            c = latencies["c"]
-            d = random.expovariate((12/1024) / c)
-            delay = rho + TX_SIZE / c + d
-            event = Event(timestamp + delay, EventType.RECEIVE_TRANSACTION, transaction, receiver.peer_id, neighbour.peer_id)
-            
+        peer.mine(block)
+        self.forward_packet(peer, block, event.timestamp, id)
+        end_mining_event = peer.start_mining(event.timestamp, hashing_power, self.I)
+        self.event_queue.add_event(end_mining_event)
 
+    def process_receive_block(self, event: Event):
+        receiver = self.peers[event.receiver]
+        block = event.data
+        
+        unseen_valid_block = receiver.receive_block(block)
+        if unseen_valid_block:
+            self.forward_packet(receiver, block, event.timestamp, event.sender)
+        
 
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='P2P Network Simulator')
-    parser.add_argument('num_peers', type=int, help='Number of peers in the network')
-    parser.add_argument('frac_slow', type=float, help='Fraction of slow peers')
-    parser.add_argument('frac_low_cpu', type=float, help='Fraction of low CPU peers')
-    parser.add_argument('interarrival_time', type=float, help='Mean interarrival time of transactions')
+    parser.add_argument('num_peers', type=int, help='Number of peers in the network', required=True)
+    parser.add_argument('frac_slow', type=float, help='Fraction of slow peers', required=True)
+    parser.add_argument('frac_low_cpu', type=float, help='Fraction of low CPU peers', required=True)
+    parser.add_argument('interarrival_time', type=float, help='Mean interarrival time of transactions', required=True)
+    parser.add_argument('I', type=float, help='Block mining time', required=True)
     args = parser.parse_args()
 
     num_peers = args.num_peers
     frac_slow = args.frac_slow
     frac_low_cpu = args.frac_low_cpu
     interarrival_time = args.interarrival_time
+    I = args.I
 
-    network = P2PNetwork(num_peers, frac_slow, frac_low_cpu)
+    network = P2PNetwork(num_peers, frac_slow, frac_low_cpu, interarrival_time, I)
     network.process_events()
 
 
