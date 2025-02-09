3492f2b6dd383eee0f28819e94ae983c8d3a7584
ShantanuWelling <210010076@iitb.ac.in>
2025-02-09 19:41:56 +0530

    script modif

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-stopping_height=20
+stopping_height=10
 
 # frac slow 0.2, 0.5, 0.8
 # frac low cpu 0.2, 0.5, 0.8
@@ -8,8 +8,8 @@ stopping_height=20
 # num peers 50, 75
 for frac_slow in 0.2 0.5 0.8; do
     for frac_low_cpu in 0.2 0.5 0.8; do
-        for I in 400 600; do
-            for interarrival_time in 100 150 200 300; do
+        for I in 100 150 200; do
+            for interarrival_time in 10 30 75 ; do
                 for num_peers in 50 80; do
                     python3 simulator.py -num_peers $num_peers -frac_slow $frac_slow -frac_low_cpu $frac_low_cpu -I $I -interarrival_time $interarrival_time -stopping_height $stopping_height &
                 done
